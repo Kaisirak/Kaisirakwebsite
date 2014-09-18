f95fb5faec29b3b4d61a4ec9756ce95fdc44c045
Kaisirak <julio.girodon@wanadoo.fr>
2014-09-17 20:38:46 -1000

    rewrwegdf

--- js/controller.js
+++ js/controller.js
@@ -38,7 +38,7 @@
 		
 		angular.element(document).ready(function () {
 			console.log("BEFORE");
-			setTimeout(function(){$scope.itemOpacity = {'opacity': 1}; console.log($scope.itemOpacity);}, 5000);
+			setTimeout(function(){$scope.itemOpacity = {'opacity': 1}; $scope.checklel();}, 5000);
 		});
 		
 	}]);
