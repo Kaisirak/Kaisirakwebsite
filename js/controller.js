05b08d550011cfedb82b36813c94dad52187e74d
Kaisirak <julio.girodon@wanadoo.fr>
2014-09-17 20:40:37 -1000

    greyhhg

--- js/controller.js
+++ js/controller.js
@@ -38,7 +38,7 @@
 		
 		angular.element(document).ready(function () {
 			console.log("BEFORE");
-			setTimeout(function(){$scope.itemOpacity = {'opacity': 1}; $scope.checklel();}, 5000);
+			$timeout(function(){$scope.itemOpacity = {'opacity': 1}; $scope.checklel();}, 5000);
 		});
 		
 	}]);
