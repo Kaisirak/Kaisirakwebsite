b687fce1615689b910dcdf8403fa6b8d55d304f2
Kaisirak <julio.girodon@wanadoo.fr>
2014-09-17 20:45:41 -1000

    grethg

--- js/controller.js
+++ js/controller.js
@@ -38,7 +38,7 @@
 		
 		angular.element(document).ready(function () {
 			console.log("BEFORE");
-			$timeout(function(){$scope.itemOpacity = {'opacity': 1}; $scope.checklel();}, 3000);
+			$timeout(function(){$scope.itemOpacity = {'opacity': 1}; $scope.checklel();}, 2000);
 		});
 		
 	}]);
